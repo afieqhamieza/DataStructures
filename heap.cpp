b50d4de1f9872bbf534d7a03b7b141a2ec18f2c1
afieqhamieza <afieqhaazemi@gmail.com>
2019-11-24 21:03:48 -0500

    reheap up and reheap down

--- heap.cpp
+++ heap.cpp
@@ -1,6 +1,6 @@
 //
 //  main.cpp
-//  Question 2
+//  reheap up and reheap down
 //
 //  Created by afieqha mieza azemi on 06/11/2019.
 //  Copyright © 2019 afieqha mieza azemi. All rights reserved.
@@ -21,9 +21,9 @@ void heaping(int arr[], int size, int i)
     if (right < size && arr[right] > arr[largest])
         largest = right;
     
-    if (largest != i) {
+    if (largest != i) 
+    {
         swap(arr[i], arr[largest]);
-        
         heaping(arr, size, largest);
     }
 }
@@ -42,7 +42,6 @@ void deleteRoot(int arr[], int& size)
     int lastElement = arr[size - 1];
     
     arr[0] = lastElement;
-    
     size = size - 1;
     
     heaping(arr, size, 0);
@@ -52,6 +51,7 @@ void printHeap(int arr[], int size)
 {
     for (int i = 0; i < size; ++i)
         cout << arr[i] << " ";
+    
     cout << "\n";
 }
 
@@ -59,19 +59,17 @@ int main(int argc, const char * argv[])
 {
     int arr[50];
     
-    for (int i=0; i<50; i++) {
+    for (int i=0; i<50; i++) 
         arr[i] = 1 + rand() % 51;
-    }
     
     int size = sizeof(arr) / sizeof(arr[0]);
-    //cout << size << endl;
     
     printHeap(arr, size);
-    
     buildHeap(arr, size);
     printHeap(arr, size);
     
-    for (int i=0; i<50 ; i++) {
+    for (int i=0; i<50 ; i++) 
+    {
         deleteRoot(arr, size);
         printHeap(arr, size);
     }
