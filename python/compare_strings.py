cb72220d3d2c467e9344a5e23234c0a59047a0ab
afieqhamieza <afieqhaazemi@gmail.com>
2022-04-13 00:39:26 -0400

    add question description

--- python/compare_strings.py
+++ python/compare_strings.py
@@ -1,5 +1,17 @@
-from ast import If
-from operator import concat
+#A string is defined to be "strictly smaller" than another string when the number of occurrences of the lexicographically smallest 
+# character in the string is less than that of the other. For example, "abcd" is strictly smaller than "aaa" because 
+# the smallest character in "abcd", "a", appears 1 time, whereas the smallest character in "aaa", "a", appears 3 times.
+
+#In another example, "d" is strictly smaller than "ff" because the smallest character in "d", 'd', appears 1 time, 
+# and the smallest character in "ff", 'f', appears 2 times.
+
+#Given a list of strings str1 with m elements, and another list of strings str2 with n elements, return an array A of n integers. 
+# For 0 <= i < n, A[i] is the number of strings in str1 that are strictly smaller than the comparison i-th string in str2. 
+# Focus on correctness instead of performance in your solution.
+
+
+
+
 from typing import List
 
 def compare_strings(str1: List[str], str2: List[str]) -> List[int]:
